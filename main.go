0ecce3cb7589ea10ad2f9dc8f44f0dba35594e0b
Md Abul Hasnat <opqclick@gmail.com>
2023-08-24 01:05:11 +0600

    spinner added to button

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 type Film struct{
@@ -15,8 +16,9 @@ type Film struct{
 func main() {
 	fmt.Println("Hello world!")
 
-	myHandler := func(w http.ResponseWriter, r *http.Request) {
+	h1 := func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("index.html"))
+
 		films := map[string][]Film{
 			"Films":{
 				{Title: "The God Father", Director: "Francis Form Coppola"},
@@ -27,7 +29,21 @@ func main() {
 		tmpl.Execute(w, films)
 	}
 
-	http.HandleFunc("/", myHandler) // Use http.HandleFunc instead of http.Handle
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+
+		htmlStr := fmt.Sprintf("<li class='list-group-item bg-primary text-white'>%s - %s</li>", title, director)
+
+		tmpl, _ := template.New("t").Parse(htmlStr)
+
+		tmpl.Execute(w, nil)
+	}
+
+	http.HandleFunc("/", h1) // Use http.HandleFunc instead of http.Handle
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
